faedf8f195aa462efe7bd33d58de4c1e3bd1d218
nimanthakasun <35260660+nimanthakasun@users.noreply.github.com>
2024-03-28 07:37:20 +0530

    Update FireBase_Util.ino

--- FireBase_Util.ino
+++ FireBase_Util.ino
@@ -3,39 +3,43 @@ bool initFirebase()
   config.api_key = FB_APIKEY;
   auth.user.email = FB_USER_EMAIL;
   auth.user.password = FB_USER_PASSW;
-  //config.token_status_callback = firestoreTokenStatuscallback;
+  config.token_status_callback = firestoreTokenStatuscallback;
   Firebase.begin(&config, &auth);
-
+  fbdo.setBSSLBufferSize(4096, 1024);
+  fbdo.setResponseSize(2048);
   return true;
 }
 
-void firestoreUpdate(float temp)
+//void create
+
+void firestoreUpdate(float t, float h, double l, int m)
 {
-  String Docpath = "plants/plant1";
+  char buf[16];
+  //long lval;
+  String macadd = WiFi.macAddress();
+  ltoa(epochTime(),buf,10);
+  String Docpath = macadd + "/" + buf;
+  //Serial.println(Docpath);
+
   FirebaseJson content;
-
-  content.set("temp/doubleValue", String(temp).c_str());
-
-  if(Firebase.Firestore.patchDocument(&fbdo, FB_PROJECT_ID, "", Docpath.c_str(), content.raw(), "temperaturey"))
+  content.set("fields/mac/stringValue", macadd);
+  //content.set("fields/timestamp/timestampValue", buf);
+  content.set("fields/temperature/doubleValue", t);
+  content.set("fields/moisture/integerValue", m);
+  content.set("fields/humidity/doubleValue", h);
+  content.set("fields/lux/doubleValue", l);
+   
+  if (Firebase.Firestore.createDocument(&fbdo, FB_PROJECT_ID,"", Docpath.c_str(), content.raw()))
   {
     Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
-    return;
-  }else
+  }
+  else
   {
     Serial.println(fbdo.errorReason());
-  }
-
-  if(Firebase.Firestore.createDocument(&fbdo, FB_PROJECT_ID, "", Docpath.c_str(), content.raw()))
-  {
-    Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
-    return;
-  }else
-  {
-      Serial.println(fbdo.errorReason());
   }
 }
 
 void firestoreTokenStatuscallback(TokenInfo info)
 {
-  //Serial.printf("Token Info: type = %s, status = %s\n", getTokenType(info).c_str(), getTokenStatus(info).c_str());
+  Serial.printf("Token Info: type = %s, status = %s\n", getTokenType(info), getTokenStatus(info));
 }
