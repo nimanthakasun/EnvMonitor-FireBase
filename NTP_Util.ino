eb921b1af1be2898a0370e5850e6da3f04fc4796
nimanthakasun <35260660+nimanthakasun@users.noreply.github.com>
2024-03-28 07:38:24 +0530

    Adds NTP_Utils

---
+++ NTP_Util.ino
@@ -0,0 +1,26 @@
+void initTime()
+{
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+}
+
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+unsigned long epochTime()
+{
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    //Serial.println("Failed to obtain time");
+    return(0);
+  }
+  time(&now);
+  return now;
+}
+
